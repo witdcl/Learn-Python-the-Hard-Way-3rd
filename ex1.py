7692e72a08278d2dbf2e1f44e9bec4cf5531aa1b
witdcl <witdcl@gmail.com>
2017-11-23 10:16:41 +0800

    ex1
    习题1

---
+++ ex1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+#ex1.py
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+# 学习心得：单引号与双引号在正常使用时功能一样，但是单引号中可以包含含有双引号的句子，反之亦然
